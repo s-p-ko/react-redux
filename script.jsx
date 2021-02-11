4526af102d1c16c766c9c1210a067118f08f8f9e
Serhii Kotsiuba <s-p-ko@ukr.net>
2021-02-11 14:41:55 +0200

    14 reducer composition with objects

--- script.jsx
+++ script.jsx
@@ -1,6 +1,6 @@
 /*
  * Open the console
- * to see that the tests pass.
+ * to see the state log.
  */
 
 const todo = (state, action) => {
@@ -39,89 +39,82 @@ const todos = (state = [], action) => {
   }
 };
 
-const testAddTodo = () => {
-  const stateBefore = [];
-  const action = {
-    type: 'ADD_TODO',
-    id: 0,
-    text: 'Learn Redux'
-  };
-  const stateAfter = [
-    {
-      id: 0,
-      text: 'Learn Redux',
-      completed: false
-    }
-  ];
-  
-  deepFreeze(stateBefore);
-  deepFreeze(action);
-  
-  expect(
-    todos(stateBefore, action)
-  ).toEqual(stateAfter);
+const visibilityFilter = (
+  state = 'SHOW_ALL',
+  action
+) => {
+  switch (action.type) {
+    case 'SET_VISIBILITY_FILTER':
+      return action.filter;
+    default:
+      return state;
+  }
 };
 
-const testToggleTodo = () => {
-  const stateBefore = [
-    {
-      id: 0,
-      text: 'Learn Redux',
-      completed: false
-    },
-    {
-      id: 1,
-      text: 'Go shopping',
-      completed: false
-    }
-  ];
-  const action = {
-    type: 'TOGGLE_TODO',
-    id: 1
+const todoApp = (state = {}, action) => {
+  return {
+    todos: todos(
+      state.todos,
+      action
+    ),
+    visibilityFilter: visibilityFilter(
+      state.visibilityFilter,
+      action
+    )
   };
-  const stateAfter = [
-    {
-      id: 0,
-      text: 'Learn Redux',
-      completed: false
-    },
-    {
-      id: 1,
-      text: 'Go shopping',
-      completed: true
-    }
-  ];
-  
-  deepFreeze(stateBefore);
-  deepFreeze(action);
-  
-  expect(
-    todos(stateBefore, action)
-  ).toEqual(stateAfter);
 };
 
+const { createStore } = Redux;
+const store = createStore(todoApp);
 
-testAddTodo();
-testToggleTodo();
-console.log('All tests passed.') || displayInPreview('All tests passed.');
+console.log('Initial state:');
+console.log(store.getState());
+console.log('--------------');
+
+console.log('Dispatching ADD_TODO.')
+store.dispatch({
+  type: 'ADD_TODO',
+  id: 0,
+  text: 'Learn Redux'
+});
+console.log('Current state:');
+console.log(store.getState());
+console.log('--------------');
+
+console.log('Dispatching ADD_TODO.');
+store.dispatch({
+  type: 'ADD_TODO',
+  id: 1,
+  text: 'Go shopping'
+});
+console.log('Current state:');
+console.log(store.getState());
+console.log('--------------');
+
+console.log('Dispatching TOGGLE_TODO.');
+store.dispatch({
+  type: 'TOGGLE_TODO',
+  id: 0
+});
+console.log('Current state:');
+console.log(store.getState());
+console.log('--------------');
+
+console.log('Dispatching SET_VISIBILITY_FILTER');
+store.dispatch({
+  type: 'SET_VISIBILITY_FILTER',
+  filter: 'SHOW_COMPLETED'
+});
+console.log('Current state:');
+console.log(store.getState());
+console.log('--------------');
 
 
 
 // display in plunker preview
 function displayInPreview(string) {
-  var newDiv = document.createElement("div"); 
-  var newContent = document.createTextNode(string); 
+  var newDiv = document.createElement("div");
+  var newContent = document.createTextNode(string);
   newDiv.appendChild(newContent);
   document.body.appendChild(newDiv)
-}
-
-// Function exported from deep-freeze lib
-function deepFreeze (o) {
-  if (o===Object(o)) {
-    Object.isFrozen(o) || Object.freeze(o)
-    Object.getOwnPropertyNames(o).forEach(function (prop) {
-      prop==='constructor'||deepFreeze(o[prop])
-    })
-  }
-  return o
 }
